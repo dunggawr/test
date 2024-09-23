1cf1c3ff03571c1ec4d747d546460a1ea25c8423
dunggawr <gawrguraholomyth@gmail.com>
2024-09-23 19:52:12 +0700

    updat

--- app/[locale]/(home)/_sections/Banner.tsx
+++ app/[locale]/(home)/_sections/Banner.tsx
@@ -23,7 +23,7 @@ export const Banner: React.FC<{ data: Section }> = ({ data }) => {
                     </div>
                 </div>
 
-                <div className="grid grid-rows-3 grid-cols-3 w-[290px] h-[290px] lg:w-[400px] lg:h-[400px] place-items-center my-8 lg:my-0">
+                <div className="grid grid-rows-3 grid-cols-3 w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] place-items-center my-8 lg:my-0">
                     <div className="w-[100px] h-[100px] md:w-[120px] md:h-[120px]"></div>
 
                     <div className="relative w-[85px] h-[85px] md:w-[120px] md:h-[120px] rounded-3xl overflow-hidden">
