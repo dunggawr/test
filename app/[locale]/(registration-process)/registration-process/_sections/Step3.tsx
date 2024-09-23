70ec9bedc8bf74ddd3a21cefefd582ab1629110c
unknown <dunggawr>
2024-09-23 19:31:40 +0700

    update

--- app/[locale]/(registration-process)/registration-process/_sections/Step3.tsx
+++ app/[locale]/(registration-process)/registration-process/_sections/Step3.tsx
@@ -10,7 +10,7 @@ export const Step3 = ({ data }: { data: any }) => {
 
   return (
     <section className="flex py-[60px] px-[16px] md:px-[106px] bg-white justify-center">
-      <div className="flex flex-col lg:flex-row place-items-center gap-11 max-w-[1228px] ">
+      <div className="flex flex-col lg:flex-row place-items-center gap-11 max-w-[1228px]">
         <div className="w-full lg:w-[393px]">
           <h2 className="text-[32px] leading-[52px] lg:text-[44px] lg:leading-[66px] font-bold md:py-[35px] md:font-bold text-black">
             {data?.name}
@@ -18,9 +18,9 @@ export const Step3 = ({ data }: { data: any }) => {
           <p className="text-[#514F4F] text-[20px] leading-8" dangerouslySetInnerHTML={{__html: data?.description}}/>
         </div>
 
-        <div className="flex relative w-[300px] h-[310px] xl:w-[557px] xl:h-[567px]">
+        <div className="flex relative w-[280px] h-[290px] xl:w-[557px] xl:h-[567px]">
           <div className="flex absolute top-0 left-0 w-full h-full justify-end items-start mr-2.5">
-            <div className="relative w-[290px] z-[2] h-[290px] rounded-[32px] xl:w-[530px] xl:h-[530px] drop-shadow-2xl overflow-hidden">
+            <div className="relative w-[270px] z-[2] h-[270px] rounded-[32px] xl:w-[530px] xl:h-[530px] drop-shadow-2xl overflow-hidden">
               <Image
                 fill
                 style={{
@@ -35,7 +35,7 @@ export const Step3 = ({ data }: { data: any }) => {
           </div>
 
           <div className="flex absolute top-0 right-0 w-full h-full justify-start items-end mr-2.5">
-            <div className="z-0 w-[290px] h-[290px] bg-[#E5BEBD] xl:w-[530px] xl:h-[530px] rounded-[32px]"></div>
+            <div className="z-0 w-[270px] h-[270px] bg-[#E5BEBD] xl:w-[530px] xl:h-[530px] rounded-[32px]"></div>
           </div>
         </div>
       </div>
--- app/[locale]/(registration-process)/registration-process/_sections/Step5.tsx
+++ app/[locale]/(registration-process)/registration-process/_sections/Step5.tsx
@@ -9,11 +9,11 @@ export const Step5: React.FC<{ data: Section }> = ({data}) => {
     const t = useTranslations("register");
 
     return (
-        <section className="py-[60px] px-[16px] md:px-[80px] bg-white">
-            <div className="grid lg:grid-cols-2 place-items-center gap-8">          
-                <div className="flex relative w-[300px] h-[310px] xl:w-[557px] xl:h-[567px]">
+        <section className="flex py-[60px] px-[16px] md:px-[106px] bg-white justify-center">
+            <div className="flex flex-col lg:flex-row place-items-center gap-11 max-w-[1228px]">          
+                <div className="flex relative w-[280px] h-[290px] xl:w-[557px] xl:h-[567px]">
                     <div className="flex absolute top-0 left-0 w-full h-full justify-end items-start mr-2.5">
-                        <div className="relative w-[290px] z-[2] h-[290px] lg:w-[530px] lg:h-[530px] rounded-[32px] drop-shadow-2xl overflow-hidden">
+                        <div className="relative w-[270px] z-[2] h-[270px] xl:w-[530px] xl:h-[530px] rounded-[32px] drop-shadow-2xl overflow-hidden">
                             <Image
                                 fill
                                 style={{ objectFit: "cover" }}
@@ -26,7 +26,7 @@ export const Step5: React.FC<{ data: Section }> = ({data}) => {
                     </div>
 
                     <div className="flex absolute top-0 right-0 w-full h-full justify-start items-end mr-2.5">
-                        <div className="z-0 w-[290px] h-[290px] lg:w-[530px] lg:h-[530px] bg-[#E5BEBD] rounded-[32px]"></div>
+                        <div className="z-0 w-[270px] h-[270px] xl:w-[530px] xl:h-[530px] bg-[#E5BEBD] rounded-[32px]"></div>
                     </div>
                 </div>
 
